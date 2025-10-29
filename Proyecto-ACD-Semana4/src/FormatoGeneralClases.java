8cca3ed5900ff0b9c6bbe9713d143cf70aeed0bb
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2025-10-29 12:21:27 -0500

    Subiendo el repo

---
+++ Proyecto-ACD-Semana4/src/FormatoGeneralClases.java
@@ -0,0 +1,16 @@
+//[nivelAcceso-de-la-clase: public, private, protected]  
+//[palabraClave-para-definir-clase: class]
+//[Nombre-de-la-Clase: NombreClase]
+public class FormatoGeneralClases 
+{ //Representa INICIO de un algoritmo
+    //Codificar TODOS METODOS deseados
+    
+    //[nivelAcceso-del-Metodo: public, private, protected] 
+    //{alcance-memoria-del-metodo: static} "opcional"
+    //[tipo-de-retorno/respuesta-del-metodo: void, int, double, boolean, ....etc.]
+    //[nombre-del-metodo: main (como metodo principal) ]
+    public static void main(String[] args) 
+    { //Represente el INICIO del metodo main    
+        //TODAS LAS SENTENCIA O CODIGO ......
+    } //Represente el FIN del metodo main
+}//Representa FIN de un algoritmo
