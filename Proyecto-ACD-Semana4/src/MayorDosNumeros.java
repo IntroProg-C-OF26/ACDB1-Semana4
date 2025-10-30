82e57cbce6db2ededfcc80b3004d13dac9146d29
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2025-10-30 11:42:28 -0500

    Subiendo el Calcular Operaciones algebraicas

--- Proyecto-ACD-Semana4/src/MayorDosNumeros.java
+++ Proyecto-ACD-Semana4/src/MayorDosNumeros.java
@@ -6,8 +6,10 @@ public class MayorDosNumeros {
         System.out.print("Dame 2 numeros: ");
         num1 = tcl.nextInt();
         num2 = tcl.nextInt();
-        if (num1 > num2)
+        if (num1 > num2){
+            System.out.println("Hola daniel");
             System.out.println(num1+" es mayor a "+num2);
+        }
         else
             System.out.println(num2+" es mayor a "+num1);
     }
